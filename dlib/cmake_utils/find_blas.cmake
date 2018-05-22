561f9c98b8a1107f535a69b4ec62e6311947cf90
Sacha <xsacha@gmail.com>
2018-05-22 14:20:40 +1000

    Hunterise

--- dlib/cmake_utils/find_blas.cmake
+++ dlib/cmake_utils/find_blas.cmake
@@ -76,28 +76,30 @@ if (UNIX OR MINGW)
       return()
    endif()
 
-   include(CheckTypeSize)
-   check_type_size( "void*" SIZE_OF_VOID_PTR)
+   if (NOT CMAKE_CROSSCOMPILING)
+      include(CheckTypeSize)
+      check_type_size( "void*" SIZE_OF_VOID_PTR)
 
-   if (SIZE_OF_VOID_PTR EQUAL 8)
-      set( mkl_search_path
-         /opt/intel/mkl/*/lib/em64t
-         /opt/intel/mkl/lib/intel64
-         /opt/intel/lib/intel64
-         /opt/intel/mkl/lib
-         )
+      if (SIZE_OF_VOID_PTR EQUAL 8)
+         set( mkl_search_path
+            /opt/intel/mkl/*/lib/em64t
+            /opt/intel/mkl/lib/intel64
+            /opt/intel/lib/intel64
+            /opt/intel/mkl/lib
+            )
 
-      find_library(mkl_intel mkl_intel_lp64 ${mkl_search_path})
-      mark_as_advanced(mkl_intel)
-   else()
-      set( mkl_search_path
-         /opt/intel/mkl/*/lib/32
-         /opt/intel/mkl/lib/ia32
-         /opt/intel/lib/ia32
-         )
+         find_library(mkl_intel mkl_intel_lp64 ${mkl_search_path})
+         mark_as_advanced(mkl_intel)
+      else()
+         set( mkl_search_path
+            /opt/intel/mkl/*/lib/32
+            /opt/intel/mkl/lib/ia32
+            /opt/intel/lib/ia32
+            )
 
-      find_library(mkl_intel mkl_intel ${mkl_search_path})
-      mark_as_advanced(mkl_intel)
+         find_library(mkl_intel mkl_intel ${mkl_search_path})
+         mark_as_advanced(mkl_intel)
+      endif()
    endif()
 
    include(CheckLibraryExists)
@@ -169,7 +171,14 @@ if (UNIX OR MINGW)
    INCLUDE (CheckFunctionExists)
 
    if (NOT blas_found)
-      find_library(cblas_lib openblas PATHS ${extra_paths})
+      # Hunter OpenBLAS not supported on these hosts/platforms for 0.2.20 or lower
+      if (IOS OR CMAKE_HOST_WIN32 OR WIN32)
+         find_library(cblas_lib openblas PATHS ${extra_paths})
+      else()
+         hunter_add_package(OpenBLAS)
+         find_package(OpenBLAS CONFIG REQUIRED)
+         set(cblas_lib OpenBLAS::OpenBLAS)
+      endif()
       if (cblas_lib)
          set(blas_libraries ${cblas_lib})
          set(blas_found 1)
